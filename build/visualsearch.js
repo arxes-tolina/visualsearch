fbc6747260e32cc0040ece985d708bc0540750e5
Philipp Eichhorn <philipp.eichhorn@arxes-tolina.de>
2016-05-20 16:44:24 +0200

    The option "customFacetValues" now marks search facet inputs as readonly.

--- build/visualsearch.js
+++ build/visualsearch.js
@@ -579,7 +579,8 @@ VS.ui.SearchFacet = Backbone.View.extend({
   render : function() {
     $(this.el).html(JST['search_facet']({
       model : this.model,
-      readOnly: this.app.options.readOnly || !this.app.options.customFacetValues
+      disabled: this.app.options.readOnly,
+      readOnly: !this.app.options.customFacetValues
     }));
 
     this.setMode('not', 'editing');
@@ -1918,6 +1919,6 @@ VS.model.SearchQuery = Backbone.Collection.extend({
 window.JST = window.JST || {};
 
 window.JST['search_box'] = _.template('<div class="VS-search <% if (readOnly) { %>VS-readonly<% } %> <% if (readOnlyFacets) { %>VS-readonly-facets<% } %>">\n  <div class="VS-search-box-wrapper VS-search-box">\n    <div class="VS-icon VS-icon-search"></div>\n    <div class="VS-placeholder"></div>\n    <div class="VS-search-inner"></div>\n    <div class="VS-icon VS-icon-cancel VS-cancel-search-box" title="clear search"></div>\n  </div>\n</div>');
-window.JST['search_facet'] = _.template('<% if (model.has(\'category\')) { %>\n  <div class="category"><%- model.get(\'category\') %>:</div>\n<% } %>\n\n<div class="search_facet_input_container">\n  <input type="text" class="search_facet_input ui-menu VS-interface" value="" <% if (readOnly) { %>disabled="disabled"<% } %> />\n</div>\n\n<div class="search_facet_remove VS-icon VS-icon-cancel"></div>');
+window.JST['search_facet'] = _.template('<% if (model.has(\'category\')) { %>\n  <div class="category"><%- model.get(\'category\') %>:</div>\n<% } %>\n\n<div class="search_facet_input_container">\n  <input type="text" class="search_facet_input ui-menu VS-interface" value="" <% if (disabled) { %>disabled="disabled"<% } %> <% if (readOnly) { %>readOnly="readOnly"<% } %> />\n</div>\n\n<div class="search_facet_remove VS-icon VS-icon-cancel"></div>');
 window.JST['search_input'] = _.template('<input type="text" class="ui-menu" <% if (readOnly) { %>disabled="disabled"<% } %> />');
 })();
